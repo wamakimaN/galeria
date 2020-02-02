690553e206228d36b25886dbeba10416b9926de1
wamakimaN <wamamoses@gmail.com>
2020-02-03 00:46:00 +0300

    install postgresql as our database dependency

--- galery/settings.py
+++ galery/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'galery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pictures',
+        'USER': 'moses',
+    'PASSWORD':'123456',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
