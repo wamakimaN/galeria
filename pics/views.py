a11e20b21723097a9039227fe4f24412352c85d5
wamakimaN <wamamoses@gmail.com>
2020-02-03 16:34:39 +0300

    add category links in navbar

--- pics/views.py
+++ pics/views.py
@@ -5,5 +5,6 @@ from .models import Picture,Category
 def home_page(request):
 
   title = 'home'
+  categories = Category.all_categs()
   pics = Picture.all_pics()
-  return render(request, 'homepage.html', {"title":title,"pics":pics})
+  return render(request, 'homepage.html', {"title":title,"pics":pics, "categories":categories})
